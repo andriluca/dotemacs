ec542e580eaecec07dc858f407ce279dea78208a
Luca Andriotto <luca.andriotto1994@gmail.com>
2023-07-20 09:24:27 +0200

    primo commit.

---
+++ init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name "config.org"
+ user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7" default))
+ '(package-selected-packages '(sly)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
